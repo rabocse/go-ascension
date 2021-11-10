b671bd8cc42faf835dc9ffa4790e68bcb3ebeab6
Alex Escobar <alexdescobars@gmail.com>
2021-11-10 15:27:00 +0100

    Fixing lasagna_test.go

---
+++ excercism/concept/lasagna/go.mod
@@ -0,0 +1,3 @@
+module github.com/rabocse/go-ascension/excercism/concept/lasagna
+
+go 1.16
--- excercism/concept/lasagna/lasagna_test.go
+++ excercism/concept/lasagna/lasagna_test.go
@@ -1,14 +1,25 @@
-func TestOvenTime (t *testing.T) {
-  
-  tt := lasagnaTests{
-                  name: "Calculates how many minutes the lasagna should be in the oven",
-    layers: 0,
-    time: 40,
-    expected: 40,
-  }
-  
-  if got := OvenTime; got != tt.expected {
-    t.Errorf("OvenTime(%d) = %d; want %d", tt.expected, got, tt.expected)
-  }
+package lasagna
+
+import "testing"
+
+type lasagnaTests struct {
+	name     string
+	layers   int
+	time     int
+	expected int
+}
+
+func TestOvenTime(t *testing.T) {
+
+	tt := lasagnaTests{
+		name:     "Calculates how many minutes the lasagna should be in the oven",
+		layers:   0,
+		time:     40,
+		expected: 40,
+	}
+
+	if got := OvenTime; got != tt.expected {
+		t.Errorf("OvenTime(%d) = %d; want %d", tt.expected, got, tt.expected)
+	}
 
 }
