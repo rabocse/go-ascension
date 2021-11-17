8b510bc39c79238a1170bcce83eac3bd2e961697
Alex Escobar <alexdescobars@gmail.com>
2021-11-17 15:10:35 +0100

    Completed first attempt of lasagna_test.go

--- excercism/concept/lasagna/lasagna_test.go
+++ excercism/concept/lasagna/lasagna_test.go
@@ -23,3 +23,47 @@ func TestOvenTime(t *testing.T) {
 	}
 
 }
+
+func TestRemainingOvenTime(t *testing.T) {
+
+	tt := lasagnaTests{
+		name:     "Calculates the remaining oven time",
+		time:     30,
+		expected: 10,
+	}
+
+	if got := RemainingOvenTime(tt.time); got != tt.expected {
+		t.Errorf("RemainingOvenTime(%d) = %d; want %d", tt.time, got, tt.expected)
+	}
+}
+
+func TestPreparationTime(t *testing.T) {
+
+	tt := lasagnaTests{
+		name:     "Calculates the preparation time depending on the amount of layers",
+		time:     2, // time = two minutes per later
+		layers:   5,
+		expected: 10,
+	}
+
+	if got := PreparationTime(tt.layers); got != tt.expected {
+		t.Errorf("PreparationTime(%d) = %d; want %d", tt.layers, got, tt.expected)
+
+	}
+
+}
+
+func TestElapseTime(t *testing.T) {
+
+	tt := lasagnaTests{
+		name:     "Calculates the elapse time",
+		time:     20, // time = time in the oven
+		layers:   2,
+		expected: 24,
+	}
+
+	if got := ElapsedTime(tt.layers, tt.time); got != tt.expected {
+		t.Errorf("ElapsedTIme(%d, %d) = %d; want %d", tt.layers, tt.time, got, tt.expected)
+	}
+
+}
